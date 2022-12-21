9535e9677057bf138fc35fd7ba092de3893f6dd6
chandni9046 <chandni9046@gmail.com>
2022-12-21 22:40:24 +0530

    home

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,10 @@ import {
 } from "react-router-dom";
 import Navbar from "./navigatiion/navbar";
 import Singin from './auth/signin/signin';
-import Home from './component/home/home';
+import Home from './component/Home/home';
 import Hotel from './component/Hotel/hotel';
+import Bus from './component/Bus/bus';
+
 
 
 function App() {
@@ -20,8 +22,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navbar />} >
-          <Route path="/" element={< Home/>} />
+          <Route path="home" element={< Home/>} />
           <Route path="hotal" element={< Hotel/>} />
+          <Route path="bus" element={< Bus/>} />
+
+
         </Route>
         <Route path="singin" element={<Singin />} />
       </Routes>
