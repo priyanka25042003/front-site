1c48edc5747cece99c324a0fc71fbd96a865aaaa
priyanka25042003 <thakorpriyanka08@gmail.com>
2022-12-27 09:40:32 -0800

    fvdfx

--- src/component/Flight/ListFlight/listflight.tsx
+++ src/component/Flight/ListFlight/listflight.tsx
@@ -43,16 +43,179 @@ function Listflight() {
   return (
     <div>
       <div>
-      <div className="dropdown">
-    <button className="btn btn-primary dropdown-toggle " type="button" data-toggle="dropdown" >Dropdown Example 
-    <span className="caret"></span></button>
-    <ul className="dropdown-menu">
-      <input className="form-control" id="myInput" type="text" placeholder="Search.."/>
-      
-    </ul>
-  </div>
-</div>
-    
+        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+          Filter
+        </button>
+
+        <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Flight Filter</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+
+                <div className="">
+                  <h5 className="">Locations</h5>
+                  <div className="row">
+                    <div className="col-sm-12">
+                      <div className="mb-2">
+                        <label id="origin-label" htmlFor="origin-input" className="form-label">From</label                    >
+                        <div className="input-group">
+                          <span className="input-group-text"><i className="bi-pin-map"></i> </span>
+                          <input
+                            type="text"
+                            className="form-control"
+                            list="origin-options"
+                            id="origin-input"
+                            placeholder="Location"
+                            aria-describedby="origin-label"
+                          />
+                          <datalist id="origin-options"></datalist>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-sm">
+                      <div className="mb-2">
+                        <label id="destination-label" htmlFor="destination-input" className="form-label">To</label                     >
+                        <div className="input-group">
+                          <span className="input-group-text"><i className="bi-pin-map-fill"></i> </span>
+                          <input
+                            type="text"
+                            className="form-control"
+                            list="destination-options"
+                            id="destination-input"
+                            placeholder="Location"
+                            aria-describedby="destination-label"
+
+                          />
+                          <datalist id="destination-options"></datalist>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="mb-2 col-sm-12">
+                    <div className="h-100 "><br />
+                      <div className="card-body">
+                        <h5 className="card-title">Dates</h5>
+                        <div className="mb-2">
+                          <label id="flight-type-label" htmlFor="flight-type-select" className="form-label"
+                          >Flight</label                  >
+                          <select
+                            id="flight-type-select"
+                            className="form-select"
+                            aria-describedby="flight-type-label"
+                          >
+                            <option value="one-way">One-way</option>
+                            <option value="round-trip">Round-trip</option>
+                            <option value="round-trip">Multi City</option>
+                          </select>
+                        </div>
+                        <div id="departure-date" className="mb-2">
+                          <label id="departure-date-label" htmlFor="departure-date-input" className="form-label"
+                          >Departure date</label                   >
+                          <div className="input-group">
+                            <span className="input-group-text"><i className="bi-calendar"></i></span>
+                            <input
+                              type="date"
+                              className="form-control"
+                              id="departure-date-input"
+                              aria-describedby="departure-date-label"
+                            />
+                          </div>
+                        </div>
+                        <div id="return-date" className="mb-2">
+                          <label id="return-date-label" htmlFor="return-date-input" className="form-label">Return date</label>
+                          <div className="input-group">
+                            <span className="input-group-text"><i className="bi-calendar-fill"></i> </span>
+                            <input
+                              type="date"
+                              className="form-control"
+                              id="return-date-input"
+                              aria-describedby="return-date-label"
+                            />
+                          </div>
+                        </div>
+                        {/* <div className="card-body">
+                          <h5 className="card-title">Details</h5>
+                          <div className="mb-2">
+                            <label id="travel-class-label" htmlFor="travel-class-select" className="form-label"
+                            >Travel class</label
+                            >
+                            <select
+                              className="form-select"
+                              id="travel-class-select"
+                              aria-describedby="travel-class-label"
+                            >
+                              <option value="ECONOMY">Economy</option>
+                              <option value="PREMIUM_ECONOMY">Premium Economy</option>
+                              <option value="BUSINESS">Business</option>
+                              <option value="FIRST">First</option>
+                            </select>
+                          </div>
+                          <label className="form-label">Passengers</label>
+                          <div className="mb-2">
+                            <div className="input-group">
+                              <label htmlFor="adults-input" className="input-group-text">Adults</label>
+                              <input
+                                type="number"
+                                min="0"
+                                className="form-control"
+                                id="adults-input"
+                                aria-describedby="adults-label"
+                              />
+                            </div>
+                            <span id="adults-label" className="form-text">12 years old and older</span>
+                          </div>
+                          <div className="mb-2">
+                            <div className="input-group">
+                              <label htmlFor="children-input" className="input-group-text">Children</label>
+                              <input
+                                type="number"
+                                min="0"
+                                className="form-control"
+                                id="children-input"
+                                aria-describedby="children-label"
+                              />
+                            </div>
+                            <span id="children-label" className="form-text">2 to 12 years old</span>
+                          </div>
+                          <div className="mb-2">
+                            <div className="input-group">
+                              <label htmlFor="infants-input" className="input-group-text">Infants</label>
+                              <input
+                                type="number"
+                                min="0"
+                                className="form-control"
+                                id="infants-input"
+                                aria-describedby="infants-label"
+                              />
+                            </div>
+                            <span id="infants-label" className="form-text">Up to 2 years old</span>
+                          </div>
+                        </div> */}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                ...
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
       <div className="d-flex mt-5  container">
         {/* <div className='w-25 h-100 bg-ight mr-5 rounded shadow-lg ' > */}
         {/* <div className="mb-3"> */}
@@ -254,6 +417,8 @@ function Listflight() {
         </div>
       </div>
     </div>
+
+
   )
 }
 
--- src/component/Hotel/ListHotel/listhotel.tsx
+++ src/component/Hotel/ListHotel/listhotel.tsx
@@ -51,8 +51,125 @@ function Listhotel() {
 
     }, [])
     return (
-        <div className="d-flex mt-5  container">
-        <div className='w-25 h-100 bg-ight mr-5 rounded shadow-lg ' >
+        <div>
+
+            <div>
+                <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                    Filter
+                </button>
+
+                <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Hotel Filter</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+
+                                <div className="">
+                                    <h5 className="">Locations</h5>
+                                    <div className="row">
+                                        <div className="col-sm-12">
+                                            <div className="mb-2">
+                                                <label id="origin-label" htmlFor="origin-input" className="form-label">From</label                    >
+                                                <div className="input-group">
+                                                    <span className="input-group-text"><i className="bi-pin-map"></i> </span>
+                                                    <input
+                                                        type="text"
+                                                        className="form-control"
+                                                        list="origin-options"
+                                                        id="origin-input"
+                                                        placeholder="Location"
+                                                        aria-describedby="origin-label"
+                                                    />
+                                                    <datalist id="origin-options"></datalist>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-sm">
+                                            <div className="mb-2">
+                                                <label id="destination-label" htmlFor="destination-input" className="form-label">To</label                     >
+                                                <div className="input-group">
+                                                    <span className="input-group-text"><i className="bi-pin-map-fill"></i> </span>
+                                                    <input
+                                                        type="text"
+                                                        className="form-control"
+                                                        list="destination-options"
+                                                        id="destination-input"
+                                                        placeholder="Location"
+                                                        aria-describedby="destination-label"
+
+                                                    />
+                                                    <datalist id="destination-options"></datalist>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="card-body">
+                                    <h5 className="card-title">Dates</h5>
+                                    <div id="Check-in" className="mb-2">
+                                        <label id="Check-in-label" htmlFor="Check-in-input" className="form-label"
+                                        >Check-in</label                   >
+                                        <div className="input-group">
+                                            <span className="input-group-text"><i className="bi-calendar"></i></span>
+                                            <input
+                                                type="date"
+                                                className="form-control"
+                                                id="Check-in-input"
+                                                aria-describedby="Check-in-label"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div id="check-out" className="mb-2">
+                                        <label id="check-out-label" htmlFor="check-out-input" className="form-label">check-out</label>
+                                        <div className="input-group">
+                                            <span className="input-group-text"><i className="bi-calendar-fill"></i> </span>
+                                            <input
+                                                type="date"
+                                                className="form-control"
+                                                id="check-out-input"
+                                                aria-describedby="check-out-label"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Details</h5>
+                                        <div className="mb-2">
+                                            <label id="travel-class-label" htmlFor="travel-class-select" className="form-label"
+                                            >hotel type</label>
+                                            <select
+                                                className="form-select"
+                                                id="travel-class-select"
+                                                aria-describedby="travel-class-label"
+                                            >
+                                                <option selected>Choose...</option>
+                                                <option value={"⭐⭐⭐⭐⭐"}>⭐⭐⭐⭐⭐</option>
+                                                <option value={"⭐⭐⭐⭐"}>⭐⭐⭐⭐</option>
+                                                <option value={"⭐⭐⭐"}>⭐⭐⭐</option>
+                                                <option value={"⭐⭐"}>⭐⭐</option>
+                                                <option value={"⭐"}>⭐</option>
+                                            </select>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                                ...
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            {/* <div className='w-25 h-100 bg-ight mr-5 rounded shadow-lg ' >
           <div className="mb-3">
             <div className="container-sm">
               <div className="my-2 ">
@@ -150,8 +267,8 @@ function Listhotel() {
                     <input className="form-range w-100" type="range" id="formFileMultiple" /><br />
                     <label htmlFor="formFileMultiple" className="form-label">PRICE</label>
                 </div>
-            </div>
-            <div className='w-75   h-100 '>
+            </div> */}
+            <div className='mt-5 h-100 mr-5'>
                 <table className="table table-hover">
                     <thead>
                         <tr>
@@ -211,7 +328,7 @@ function Listhotel() {
                 </table>
             </div>
         </div>
-        </div >
+
     )
 }
 
