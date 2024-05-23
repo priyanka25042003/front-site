39065b823c9be66230a7f58da8fb22c58c841336
sumit patel <84306168+sumit75757@users.noreply.github.com>
2024-05-23 10:52:03 +0530

    Update firebase.tsx

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -6,14 +6,7 @@ import firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCjdBef7YjTT_1JxkIX34okuy0Ly-6pfC0",
-  authDomain: "tour-and-travel-agency-3eb3a.firebaseapp.com",
-  databaseURL:
-    "https://tour-and-travel-agency-3eb3a-default-rtdb.firebaseio.com",
-  projectId: "tour-and-travel-agency-3eb3a",
-  storageBucket: "tour-and-travel-agency-3eb3a.appspot.com",
-  messagingSenderId: "698063399359",
-  appId: "1:698063399359:web:8758504db66b59df2d590a",
+  // set firabse config hear
 };
 
 export const app = firebase.initializeApp(firebaseConfig);
